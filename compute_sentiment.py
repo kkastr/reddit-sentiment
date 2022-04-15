9987492137759d1c6e30e80d6e57ab59417af70d
kkastr <dinokastritis@gmail.com>
2022-04-15 16:09:53 -0400

    added functionality to scrape multiple subreddits

--- compute_sentiment.py
+++ compute_sentiment.py
@@ -1,67 +1,100 @@
 import re
+from turtle import title
 import nltk
 import nltk.sentiment.vader as sev
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+# from wordcloud import WordCloud
+from textblob import TextBlob
 
-
-
-# pre-trained sentiment stuff
+# pre-trained sentiment analysis
 # nltk.download('vader_lexicon', download_dir='./')
 
-datafile = f'./data/comments.csv'
+# subredditName = 'news'
+listOfSubreddits = ['games', 'news', 'science', 'space', 'politics', 'gonewild']
 
-df = pd.read_csv(datafile)
+for subredditName in listOfSubreddits:
+    datafile = f'./data/{subredditName}_comments.csv'
 
-pf = pd.read_csv(f'./data/posts.csv')
+    df = pd.read_csv(datafile)
 
-df['body'] = df['body'].str.replace("\W", " ", regex=True)
-df['body'] = df['body'].str.replace("\s+", " ", regex=True)
-df['body'] = df['body'].str.replace("\s+[a-zA-Z]\s+", " ", regex=True)
-df['body'] = df['body'].str.replace("\^[a-zA-Z]\s+", " ", regex=True)
-df['body'] = df['body'].str.lower()
+    pf = pd.read_csv(f'./data/{subredditName}_posts.csv')
 
-pf['title'] = pf['title'].str.replace("\W", " ", regex=True)
-pf['title'] = pf['title'].str.replace("\s+", " ", regex=True)
-pf['title'] = pf['title'].str.replace("\s+[a-zA-Z]\s+", " ", regex=True)
-pf['title'] = pf['title'].str.replace("\^[a-zA-Z]\s+", " ", regex=True)
-pf['title'] = pf['title'].str.lower()
+    # df['body'] = df['body'].str.replace("\W", " ", regex=True)
+    # df['body'] = df['body'].str.replace("\s+", " ", regex=True)
+    # df['body'] = df['body'].str.replace("\s+[a-zA-Z]\s+", " ", regex=True)
+    # df['body'] = df['body'].str.replace("\^[a-zA-Z]\s+", " ", regex=True)
+    # df['body'] = df['body'].str.lower()
+
+    # pf['title'] = pf['title'].str.replace("\W", " ", regex=True)
+    # pf['title'] = pf['title'].str.replace("\s+", " ", regex=True)
+    # pf['title'] = pf['title'].str.replace("\s+[a-zA-Z]\s+", " ", regex=True)
+    # pf['title'] = pf['title'].str.replace("\^[a-zA-Z]\s+", " ", regex=True)
+    # pf['title'] = pf['title'].str.lower()
+
+    sia = sev.SentimentIntensityAnalyzer()
+
+    sia_cols = ['comment_sentiment', 'title_sentiment', 'comment_score', 'post_score', 'post_id']
+
+    gb = df.groupby('post_id')
+
+    flist = []
+    for key, grp in gb:
+        tdf = pd.DataFrame(columns=sia_cols)
+
+        for item in grp.iterrows():
+            post = pf[pf.id == key]
+
+            idx = item[0]
+            comment = item[1]
+            title_sentiment = sia.polarity_scores(post.title.values[0])['compound']
+            sentiment_score = sia.polarity_scores(comment.body)['compound']
 
 
-sia = sev.SentimentIntensityAnalyzer()
+            tdf.loc[idx, sia_cols] = [sentiment_score, title_sentiment, comment.score,
+                                      post.score.values[0], key]
+        flist.append(tdf)
 
-sia_cols = ['sentiment', 'title_sentiment', 'score', 'post_score', 'post_id']
+    cdf = pd.concat(flist, ignore_index=True)
 
-gb = df.groupby('post_id')
+    grp = cdf.groupby('post_id')
 
-flist = []
-for key, grp in gb:
-    tdf = pd.DataFrame(columns=sia_cols)
+    # print(cdf.post_id.unique())
+    # for k, g in grp:
+    #     plt.hist(g.comment_sentiment, bins=50)
+    #     plt.show()
 
-    for item in grp.iterrows():
-        post = pf[pf.id == key]
 
-        idx = item[0]
-        comment = item[1]
-        title_sentiment = sia.polarity_scores(post.title.values[0])['compound']
-        sentiment_score = sia.polarity_scores(comment.body)['compound']
-
-        tdf.loc[idx, sia_cols] = [sentiment_score, title_sentiment, comment.score, pf[pf.id==key].score.values[0], key]
-    flist.append(tdf)
-
-    # plt.scatter(tdf.score, tdf.sentiment)
-    # plt.hist(tdf.sentiment)
+    # plt.hist(cdf.comment_sentiment)
     # plt.show()
 
-cdf = pd.concat(flist, ignore_index=True)
-# plt.hist(cdf.sentiment)
-# plt.show()
+    #
+
+    sns.histplot(cdf.comment_sentiment, kde=True, stat='probability', label=subredditName)
+    # sns.kdeplot(cdf.comment_sentiment, label=subredditName)
+    # sc = grp.std().comment_sentiment
+
+    # st = grp.std().score
+
+    # plt.scatter(cdf.comment_score, cdf.comment_sentiment)
+    # plt.show()
+    print(f'finished with r/{subredditName}')
+
+    # for key, grp in gb:
+    #     text = grp.body
+
+    #     wc = WordCloud().generate(text)
+
+    #     plt.imshow(wc, interpolation='bilinear')
+    #     plt.axis('off')
+    #     plt.show()
+    #     break
 
 
-# plt.scatter(cdf.sentiment, cdf.post_score)
-# plt.show()
-
+plt.legend()
+plt.show()
 # cav = cdf.groupby('post_id').median()
 
 # print(cav)
--- scrape_data.py
+++ scrape_data.py
@@ -7,6 +7,9 @@ import vutils.general as vg
 
 import sqlite3
 
+from api_keys import client_id, client_secret, user_agent, username
+
+
 @vg.timing
 def get_posts(subreddit, num_posts: int, output: str):
 
@@ -15,12 +18,17 @@ def get_posts(subreddit, num_posts: int, output: str):
 
     posts_df = pd.DataFrame(columns=post_columns)
 
-    for idx, post in enumerate(subreddit.hot(limit=num_posts)):
+    for post in subreddit.hot(limit=num_posts):
 
-        posts_df.loc[idx, post_columns] = [post.title, post.score, post.upvote_ratio, post.id,
-                                           post.subreddit, post.url, post.num_comments,
-                                           post.is_reddit_media_domain, post.total_awards_received,
-                                           post.created]
+        if post.stickied:
+            continue
+
+        posts_df.loc[len(posts_df), post_columns] = [post.title, post.score, post.upvote_ratio,
+                                                     post.id, post.subreddit, post.url,
+                                                     post.num_comments,
+                                                     post.is_reddit_media_domain,
+                                                     post.total_awards_received,
+                                                     post.created]
 
     posts_df.to_csv(output, index=False)
 
@@ -46,10 +54,15 @@ def get_comments(reddit, id_list, output):
 
         comments_df = pd.DataFrame(columns=comment_columns)
 
-        for idx, comment in enumerate(submission.comments.list()):
+        for comment in submission.comments.list():
 
-            comments_df.loc[idx, comment_columns] = [comment.body, comment.score, post_id,
-                                                     comment.total_awards_received, comment.created]
+            if comment.stickied:
+                continue
+
+            comments_df.loc[len(comments_df), comment_columns] = [comment.body, comment.score,
+                                                                  post_id,
+                                                                  comment.total_awards_received,
+                                                                  comment.created]
 
         flist.append(comments_df)
 
@@ -60,10 +73,6 @@ def get_comments(reddit, id_list, output):
 
 def main():
 
-    client_id = 'gYOYg_jmapcVVZOyEV08pw'
-    client_secret = 'VzPlZrSxV6hrzlcrP47nQn-1G9Dliw'
-    user_agent = 'generate_data_for_nlp_project'
-    username = 'symmetryconserved'
     reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
                          user_agent=user_agent, username=username)
 
@@ -76,19 +85,20 @@ def main():
 
     sp.run(full_cmd.split())
 
-    subreddit = reddit.subreddit('games')
-
-    # posts_outfile = f'{dir_name}/r_{subreddit.display_name}-posts.csv'
-    # comments_outfile = f'{dir_name}/r_{subreddit.display_name}-comments.csv'
-
-    posts_outfile = f'{dir_name}/posts.csv'
-    comments_outfile = f'{dir_name}/comments.csv'
+    listOfSubreddits = ['games', 'news', 'science', 'space', 'politics', 'gonewild']
 
     num_posts = 10
 
-    df = get_posts(subreddit=subreddit, num_posts=num_posts, output=posts_outfile)
+    for subredditName in listOfSubreddits:
 
-    get_comments(reddit=reddit, id_list=df.id.values, output=comments_outfile)
+        subreddit = reddit.subreddit(subredditName)
+
+        posts_outfile = f'{dir_name}/{subredditName}_posts.csv'
+        comments_outfile = f'{dir_name}/{subredditName}_comments.csv'
+
+        df = get_posts(subreddit=subreddit, num_posts=num_posts, output=posts_outfile)
+
+        get_comments(reddit=reddit, id_list=df.id.values, output=comments_outfile)
 
 
 if __name__ == "__main__":
