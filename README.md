9ecdf657a006f9196a3d3782546e60c39d9c0f7a
kkastr <dinokastritis@gmail.com>
2022-06-28 15:51:40 -0400

    update readme and add plot to repo

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# reddit comments sentiment analysis
+# Usage
+
+The code is run in multiple steps:
+
+First, obtain the relevant api keys from reddit ([instructions](https://docs.aws.amazon.com/solutions/latest/discovering-hot-topics-using-machine-learning/retrieve-and-manage-api-credentials-for-reddit-api-authentication.html)) and place them in a file named `api_keys.py` as such:
+
+```python
+client_id = 'your-client-id'
+client_secret = 'your-client-key'
+user_agent = 'your-app-name'
+username = 'your-reddit-username'
+```
+
+Next, in `params.py`, specify the number of posts and which subreddits you wish to scrape.
+
+After the setup is complete, successively run the following in your terminal:
+
+```bash
+python3 scrape_data.py
+```
+
+```bash
+python3 compute_sentiment.py
+```
+
+Once the computation has finished, the script will generate a file called `sentiment.csv` which will contain all the sentiment data in addition to some other information, such as the time of posting, and the score.
+
+## Sentiment analysis on reddit comments
+
+Sentiment analysis is the process of programmatically extracting sentiment from text using methods from natural language processing (NLP). The primary method employed in the analysis is the characterization of a piece of text by its polarity, a label that indicates if the text was negative, neutral, or positive (there can be more gradations than these three). Sentiment analysis has been used for several years, with great success, for determining brand sentiment, user response, and more.
+
+In this work, we look at the comments from various subreddits and compute the sentiment which provides some insight on the discourse within those subreddits.
 
 ![sent_dist](./plots/sentiment_percent_per_sub.png)
+
+
+![av_sent_time](./plots/comment_density_v_time_after_submission.png)
--- plot.py
+++ plot.py
@@ -6,82 +6,95 @@ import seaborn as sns
 import datetime
 import os
 
-os.system('mkdir -p ./plots/')
+fontsize = 16
 
-plt.rc('font', family='serif', size=16)
-plt.rc('lines', linewidth=4, aa=True)
-plt.rc('figure', figsize=(10, 6))
+os.system("mkdir -p ./plots/")
 
-df = pd.read_csv('sentiment_data.csv')
+plt.rc("font", family="serif", size=fontsize)
+plt.rc("lines", linewidth=4, aa=True)
+plt.rc("figure", figsize=(10, 6))
+
+df = pd.read_csv("sentiment_data.csv")
 
 colors = iter(list(mcolors.TABLEAU_COLORS))
 
-hist_kwargs = dict(kde=True, stat='density', alpha=0, edgecolor=None)
+hist_kwargs = dict(kde=True, stat="density", alpha=0, edgecolor=None)
 
-df.comment_time = pd.to_datetime(df.comment_time, unit='s')
+df.comment_time = pd.to_datetime(df.comment_time, unit="s")
 
-df.post_time = pd.to_datetime(df.post_time, unit='s')
+df.post_time = pd.to_datetime(df.post_time, unit="s")
 
-df['timedelta'] = [item.seconds / 60 / 60 for item in df.comment_time - df.post_time]
+df["timedelta"] = [item.seconds / 60 / 60 for item in df.comment_time - df.post_time]
 
-df['timeofday'] = [item.hour for item in df.comment_time]
+df["timeofday"] = [item.hour for item in df.comment_time]
 
-df['slabel'] = ['Positive' if v > 0.1 else 'Negative' if v < -0.1
-                else 'Neutral' for v in df.comment_sentiment]
+df["slabel"] = [
+    "Positive" if v > 0.1 else "Negative" if v < -0.1 else "Neutral" for v in df.comment_sentiment
+]
 
-df['hrafter'] = np.round(df.timedelta.values)
+df["hrafter"] = np.round(df.timedelta.values)
 
-subreddit_order = ['games', 'news', 'politics', 'science', 'space']
+subreddit_order = ["games", "news", "politics", "science", "space"]
 
-ax = sns.kdeplot(x=df.comment_sentiment, hue=df.subreddit, hue_order=subreddit_order,
-                 cut=0, common_norm=False)
-sns.move_legend(ax, 'upper left', bbox_to_anchor=(1.04, 1), frameon=False,)
-ax.set_ylabel('Comment Density')
-ax.set_xlabel('Sentiment Value')
+ax = sns.kdeplot(
+    x=df.comment_sentiment, hue=df.subreddit, hue_order=subreddit_order, cut=0, common_norm=False
+)
+sns.move_legend(ax, "upper left", bbox_to_anchor=(1.04, 1), frameon=False)
+ax.set_ylabel("Comment Density")
+ax.set_xlabel("Sentiment Value")
 plt.tight_layout()
-plt.savefig('./plots/comment_density_vs_sent_value.png', dpi=600)
+plt.savefig("./plots/comment_density_vs_sent_value.png", dpi=600)
 plt.show()
 
-ax = sns.kdeplot(x=df.timeofday, hue=df.subreddit, hue_order=subreddit_order,
-                 cut=0, common_norm=False)
-sns.move_legend(ax, 'upper left', bbox_to_anchor=(1.04, 1), frameon=False,)
-ax.set_ylabel('Comment Density')
-ax.set_xlabel('Time of Day (hours)')
+ax = sns.kdeplot(
+    x=df.timeofday, hue=df.subreddit, hue_order=subreddit_order, cut=0, common_norm=False
+)
+sns.move_legend(ax, "upper left", bbox_to_anchor=(1.04, 1), frameon=False)
+ax.set_ylabel("Comment Density")
+ax.set_xlabel("Time of Day (hours)")
 plt.tight_layout()
-plt.savefig('./plots/submission_density_per_time.png', dpi=600)
+plt.savefig("./plots/submission_density_per_time.png", dpi=600)
 plt.show()
 
-gb = df.groupby('subreddit').slabel.value_counts(normalize=True).rename('percent').reset_index()
-fgrid = sns.catplot(x='slabel', y='percent', hue='subreddit', hue_order=subreddit_order,
-                    order=['Negative', 'Neutral', 'Positive'], kind='bar', data=gb)
-fgrid.axes[0, 0].set_ylabel('Percent of Total')
-fgrid.axes[0, 0].set_xlabel('')
-plt.savefig('./plots/sentiment_percent_per_sub.png', dpi=600)
+gb = df.groupby("subreddit").slabel.value_counts(normalize=True).rename("percent").reset_index()
+fgrid = sns.catplot(
+    x="slabel",
+    y="percent",
+    hue="subreddit",
+    hue_order=subreddit_order,
+    order=["Negative", "Neutral", "Positive"],
+    kind="bar",
+    data=gb,
+)
+fgrid.axes[0, 0].set_ylabel("Percent of Total")
+fgrid.axes[0, 0].set_xlabel("")
+plt.savefig("./plots/sentiment_percent_per_sub.png", dpi=600)
 plt.show()
 
-gb = df.groupby(['timeofday', 'subreddit']).comment_sentiment.mean().rename('avS').reset_index()
-ax = sns.lineplot(x='timeofday', y='avS', hue='subreddit', hue_order=subreddit_order, data=gb)
-sns.move_legend(ax, 'upper left', bbox_to_anchor=(1.04, 1), frameon=False,)
-ax.set_ylabel('Average Sentiment')
-ax.set_xlabel('Time of Day (hours)')
+gb = df.groupby(["timeofday", "subreddit"]).comment_sentiment.mean().rename("avS").reset_index()
+ax = sns.lineplot(x="timeofday", y="avS", hue="subreddit", hue_order=subreddit_order, data=gb)
+sns.move_legend(ax, "upper left", bbox_to_anchor=(1.04, 1), frameon=False)
+ax.set_ylabel("Average Sentiment")
+ax.set_xlabel("Time of Day (hours)")
 plt.tight_layout()
-plt.savefig('./plots/average_sentiment_per_time.png', dpi=600)
+plt.savefig("./plots/average_sentiment_per_time.png", dpi=600)
 plt.show()
 
-ax = sns.kdeplot(x='timedelta', hue='subreddit', hue_order=subreddit_order, data=df,
-                 cut=0, common_norm=False)
-sns.move_legend(ax, 'upper left', bbox_to_anchor=(1.04, 1), frameon=False,)
-ax.set_ylabel('Comment Density')
-ax.set_xlabel('Hours After Post Submission')
+ax = sns.kdeplot(
+    x="timedelta", hue="subreddit", hue_order=subreddit_order, data=df, cut=0, common_norm=False
+)
+sns.move_legend(ax, "upper left", bbox_to_anchor=(1.04, 1), frameon=False)
+ax.set_ylabel("Comment Density")
+ax.set_xlabel("Hours After Post Submission")
 plt.tight_layout()
-plt.savefig('./plots/comment_density_v_time_after_submission.png', dpi=600)
+plt.savefig("./plots/comment_density_v_time_after_submission.png", dpi=600)
 plt.show()
 
-gb = df.groupby(['hrafter', 'subreddit']).comment_sentiment.mean().rename('avS').reset_index()
-ax = sns.lineplot(x='hrafter', y='avS', hue='subreddit', hue_order=subreddit_order, data=gb)
-sns.move_legend(ax, 'upper left', bbox_to_anchor=(1.04, 1), frameon=False,)
-ax.set_ylabel('Average Sentiment')
-ax.set_xlabel('Hours After Post Submission')
+gb = df.groupby(["hrafter", "subreddit"]).comment_sentiment.mean().rename("avS").reset_index()
+ax = sns.lineplot(x="hrafter", y="avS", hue="subreddit", hue_order=subreddit_order, data=gb)
+sns.move_legend(ax, "upper left", bbox_to_anchor=(1.04, 1), frameon=False)
+ax.set_ylabel("Average Sentiment")
+ax.set_xlabel("Hours After Post Submission")
 plt.tight_layout()
-plt.savefig('./plots/average_sentiment_per_hr_after.png', dpi=600)
+plt.savefig("./plots/average_sentiment_per_hr_after.png", dpi=600)
 plt.show()
